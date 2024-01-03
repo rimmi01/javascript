b12183c01f319a9b2055f6b8b986e732a2bd3bc1
Rimmi Goyal <134063409+rimmi01@users.noreply.github.com>
2024-01-03 23:10:53 +0000

    datatypes-summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,46 @@
+// Primitive 
+
+// 7 types : String, Number, Boolean, null, undefined, symbol, BigInt
+
+const score = 100
+// console.log(typeof score); //number
+
+const scoreValue = 100.3
+// console.log(typeof scoreValue);   // number
+
+const isLoggedIn = false
+// console.log(typeof isLoggedIn);// boolean
+
+const outsideTemp = null
+// console.log(typeof outsideTemp);   // object
+
+let userEmail;     // undefined
+// console.log(typeof userEmail);   // undefined
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+// console.log(typeof id, typeof anotherId);   // symbol symbol
+
+// console.log(id === anotherId);
+
+const bigNumber = 32425465465439735943759n
+// console.log(typeof bigNumber);    // bigint
+
+
+// Reference (Non primitive)
+
+//Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"]
+let myObj = {
+    name: "hitesh",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
+
