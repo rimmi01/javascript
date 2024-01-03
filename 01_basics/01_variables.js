78a7fb64ad16121e3ef3ffec2f086b2f76473256
Rimmi Goyal <134063409+rimmi01@users.noreply.github.com>
2024-01-03 12:01:42 +0000

    commited some changes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 144553;
+let accountEmail = "goyalrimmi12@gmail.com";
+var accountPassword = "12345";
+accountCity = "Jaipur";
+let accountState;
+// accountId = 2    // not allowed
+accountEmail = "hc@gmail.com";
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
+/*
+Prefer not to use var 
+because of issue in block scope and functional scope 
+*/
