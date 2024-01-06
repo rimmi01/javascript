b8293fb751efc2843b8d8cb8b202dc9ae4ebd2e4
Rimmi Goyal <134063409+rimmi01@users.noreply.github.com>
2024-01-06 06:04:33 +0000

    date module, remove some files and folders

--- 01_basics/07_datesinJs.js
+++ 01_basics/07_datesinJs.js
@@ -18,6 +18,16 @@ let myCreatedDate = new Date("2023-01-14");
 
 let myTimeStamp = Date.now()
 
-console.log(myTimeStamp);
-console.log(myCreatedDate.getTime());
-console.log(Date.now()/1000);
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date()
+console.log(newDate.getMonth());
+console.log(newDate.getDay());
+
+// `${newDate.getDay()} and the time`
+
+newDate.toLocaleString('default', {
+    weekday: "long",
+})
